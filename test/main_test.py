a89413898cd453ee1277f6363949205a6bcb0b1f
Simon Vestergaard Johansen <SERJA@vestas.com>
2024-08-20 14:39:48 +0200

    adding test that only passes on 3.12.0+

--- test/main_test.py
+++ test/main_test.py
@@ -1,5 +1,17 @@
 from pythonpipelinedemo.main import add_one
+import sys
+from packaging import version
 
 
 def test_main():
     assert add_one(1) == 2
+
+
+def test_specific_python_version():
+    current_version = sys.version.split(" ")[0]
+    min_version = "3.12.0"
+
+    # Assert that the current Python version is greater than the minimum required version
+    assert version.parse(current_version) > version.parse(
+        min_version
+    ), f"Python version must be greater than {min_version}. Current version: {current_version}"
